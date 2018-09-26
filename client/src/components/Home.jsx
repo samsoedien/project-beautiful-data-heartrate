c5914107d589b1f17ebac350c43f3a24a88c76e1
Samsoedien <samsoedien@Samsoediens-MacBook-Pro.local>
2018-09-27 00:28:08 +0200

    change url

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1>Instructions</h1>
       <div className="container">
         <p className="lead">This is a data collector and visualisation for stress related to heart data. In the User form irregular BPM values are filter and the user can fill in some contextual data to enrich the dataset.</p>   
-        <p className="">The application is build in react and is connected to a Rest API. Go to <a href="localhost:4000/api/heartdata">localhost:4000/api/heartdata</a> to view heartdata. Post request can be send to this end-point (bpm: Number).</p>    
+        <p className="">The application is build in react and is connected to a Rest API. Go to <a href="https://beautiful-data.herokuapp.com/api/heartdata">https://beautiful-data.herokuapp.com/api/heartdata</a> to view heartdata. Post request can be send to this end-point (bpm: Number).</p>    
       </div>
     </div>
     
