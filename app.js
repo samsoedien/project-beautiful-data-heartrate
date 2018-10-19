c94204bc0625471c5467ea1279d68868861527da
Samsoedien <samsoedien@wlan-202099.nbw.tue.nl>
2018-10-19 08:26:13 +0200

    change app.js build settings

--- app.js
+++ app.js
@@ -67,10 +67,12 @@ app.use((error, req, res, next) => {
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  //app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
