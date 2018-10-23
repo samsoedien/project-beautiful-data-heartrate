856df351575d6453806b54084e82c8ae8889ef10
Samsoedien <samsoedien@wlan-202220.nbw.tue.nl>
2018-10-23 11:01:42 +0200

    vega implementations

--- client/src/components/Visualisation.jsx
+++ client/src/components/Visualisation.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import vegalite from "vega-lite";
 import vegaEmbed from "vega-embed";
 
+import LINE_SPEC from "./line.vl.json";
+
 import FakeDataCollector from "./FakeDataCollector";
 
 class Visualisation extends Component {
   componentDidMount() {
-    var vlSpec = "bar.vl.json";
+    var vlSpec = LINE_SPEC;
     vegaEmbed("#vis", vlSpec);
   }
 
